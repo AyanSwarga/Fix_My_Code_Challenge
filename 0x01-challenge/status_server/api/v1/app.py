bd2acb8eb88b7f7d93c285abdd6f6d9d12c588ae
AyanSwarga <ayannicholas@gmail.com>
2023-07-17 16:18:45 +0100

    fix update

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -2,11 +2,14 @@
 """
 Web server
 """
-from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+# Error handler code and the __name__ == "__main__" block as before
+
 
 
 @app.route('/status', methods=['GET'])
